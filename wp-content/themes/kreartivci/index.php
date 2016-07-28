1cf22a7c4e8f904db778c15f58fdc0fef37bc607
kekebeen <kakosi69@gmail.com>
2016-07-28 16:27:19 +0200

    Added skeleton hero section with title

--- wp-content/themes/kreartivci/index.php
+++ wp-content/themes/kreartivci/index.php
@@ -2,6 +2,7 @@
   
     <?php include('partials/home/udruzenje-nav.php') ?>
 	<div id="main">
+        <?php include('partials/home/hero.php'); ?>
         <div class="inner">
 
         
---
+++ wp-content/themes/kreartivci/partials/home/hero.php
@@ -0,0 +1,6 @@
+<div class="hero">
+    <h1>
+        <span>Loser friendly</span><br>
+        Batman Nav
+    </h1>
+</div>
