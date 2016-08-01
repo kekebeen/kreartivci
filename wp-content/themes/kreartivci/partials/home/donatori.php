10fe82939e9b304b562f1b5150c1ad7c5c9e98d7
kekebeen <kakosi69@gmail.com>
2016-08-01 16:09:07 +0200

    Added donatori section with donator logo's

--- wp-content/themes/kreartivci/partials/home/donatori.php
+++ wp-content/themes/kreartivci/partials/home/donatori.php
@@ -1,4 +1,18 @@
-<section id="donatori" class="donatorio-section">
+<section id="donatori" class="donatori-section">
     <h1>Donatori</h1>
-    
+
+    <div class="row donatori-row">
+        <div class="donator column">
+            <img src="<?php echo get_bloginfo('template_directory');?>/images/logo1.png" alt="google ventures">
+        </div>
+        <div class="donator column">
+            <img src="<?php echo get_bloginfo('template_directory');?>/images/logo2.png" alt="google ventures">
+        </div>
+        <div class="donator column">
+            <img src="<?php echo get_bloginfo('template_directory');?>/images/logo3.png" alt="susa ventures">
+        </div>
+        <div class="donator column">
+            <img src="<?php echo get_bloginfo('template_directory');?>/images/logo4.png" alt="SVAngel">
+        </div>
+    </div>
 </section><!-- donatori section -->
